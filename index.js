331fd25b8cdb08e188c8e2a1f17f8d2e38acc116
Azpil <eazpil01@gmail.com>
2026-03-25 21:49:27 -0600

    Feat. Add a display list of the all states

--- index.js
+++ index.js
@@ -1,99 +1,55 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mysql from "mysql2/promise";
-import { createRequire } from "node:module";
 import "dotenv/config";
-import { get } from "node:http";
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-const require = createRequire(import.meta.url);
-
-// let theUser;
-// let thePass;
 let currentUserId = 1;
 
-// try {
-//   const local = require("./config.locals.cjs");
-//   theUser = process.env.DB_USER || local.USER;
-//   thePass = process.env.DB_PASSWORD || local.PASSWORD;
-// } catch (error) {
-//   console.error("Error al cargar config.locals.cjs", error.message);
-// }
+const theUser = process.env.DB_USER; //Configuramos las constantes del entorno usando el archivo .env
+const thePass = process.env.DB_PASSWORD; //En este archivo se asigna el valor y se sube directamente al host
+//Hacemos esto para que nuestras credenciales no se suban a github ya que están en el archivo gitignore
 
-const theUser = process.env.DB_USER;
-const thePass = process.env.DB_PASSWORD;
-
-if (!theUser || !thePass) {
-  console.error("ERROR: Faltan las credenciales en las variables de entorno.");
+if (!theUser || !thePass) { //Comprobamos que existan estas variables
+  console.error("ERROR: Faltan las credenciales en las variables de entorno."); 
 }
 
-// const connection = await mysql.createConnection({
-//   host: "srv1293.hstgr.io",
-//   user: theUser,
-//   database: "u354636099_test1",
-//   password: thePass,
-// });
+let connection;  //Se declara esta variable para que se convierta en una variable global
 
-
-// //-
-// let connection;
-
-
-// async function initializeDB() {
-//   try {
-//     connection = await mysql.createConnection({
-//       host: "srv1293.hstgr.io",
-//       user: theUser,
-//       database: "u354636099_test1",
-//       password: thePass,
-//     });
-//     console.log("Conectado a la base de datos de Hostinger");
-//   } catch (err) {
-//     console.error("Error inicializando la BD:", err);
-//   }
-// }
-
-// initializeDB();
-// //-
-
-//*
-let connection;
-
-async function initializeDB() {
- connection = await mysql.createConnection({
- host: "srv1293.hstgr.io",
- user: theUser,
- database: "u354636099_test1",
- password: thePass,
- });
- console.log("Conectado a la base de datos de Hostinger");
+async function initializeDB() { //Esta función asíncrona se encarga de realzar la conexión a la base de datos 
+  connection = await mysql.createConnection({ //con 'await' se indica que tiene que esperar a crear la conexión
+    host: "srv1293.hstgr.io", //y le pasamos los valores para crear la conexión
+    user: theUser,
+    database: "u354636099_test1",
+    password: thePass,
+  });
+  console.log("Conectado a la base de datos de Hostinger"); //Por último le indicamos que ya se hizo la conexión
 }
 
-(async () => {
- try {
- await initializeDB(); // <- clave
- app.listen(3000, () => console.log("All ok from port 3000"));
- } catch (err) {
- console.error("Error inicializando la BD:", err);
- process.exit(1); // <- evita que atienda requests sin BD
- }
-})();
-//*
+(async () => { //Esta es una función autoejecutable  o IIFE: Inmediatly Invoked Function Expression
+  try {
+    await initializeDB(); // <- clave --Espera a inicializar la base de datos
+    app.listen(3000, () => console.log("All ok from port 3000")); //Inicializa la aplicación
+  } catch (err) { //En caso de error
+    console.error("Error inicializando la BD:", err); //Nos manda a la consola el error
+    process.exit(1); // <- evita que atienda requests sin BD
+  }
+})(); //*En el primer paréntesis se declara como una función normal y el segundo le indica a express que la ejecute. Las funciones autoejecutables 
+//* tiene dos paréntesis
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true })); //La app usa esto para poder leer lo que el usuario introduce en el html o ejs
+app.use(express.static("public")); //Se le indica la carpeta donde estarán los archivos
 
-let users = [
-  { id: 1, name: "Azpil1", color: "teal" },
+let users = [ //Este es un objeto que nos sirve como referencia para saber cómo está configurada la tabla de la base de datos
+  { id: 1, name: "Azpil1", color: "teal" }, //Cada registro de la tabla de la base de datos tiene un id, nombre y color
   { id: 2, name: "Azpil2", color: "powderblue" },
 ];
 
-async function getDataCurrentUser() {
+async function getDataCurrentUser() {  //Esta es la primera función que nos ayuda a 
   const result = await connection.query("SELECT * FROM users");
   users = result[0];
-  const theUser = users.find((user) => user.id == currentUserId);
+  const theUser = users.find((user) => user.id == currentUserId); //*Esto nos ayuda a comprar valores similares en vez de idénticos. (2 == "2") True / (2 === "2") False
   return theUser;
 }
 
@@ -117,6 +73,19 @@ async function checkVisited() {
   return listaEstados;
 }
 
+async function getAllStateNames() {
+    const estadosMex = []
+    const result = await connection.query(
+      "SELECT Nombre FROM estadosMex"
+    );
+    const lista = result[0];
+    lista.forEach((estado) => {
+      estadosMex.push(estado.Nombre);
+    })
+    return estadosMex;
+  
+}
+
 app.get("/test", async (req, res) => {
   console.log(await getDataCurrentUser());
 });
@@ -124,13 +93,16 @@ app.get("/test", async (req, res) => {
 app.get("/", async (req, res) => {
   const currentUser = await getDataCurrentUser();
   const estados = await checkVisited();
+  const todosEstados = await getAllStateNames();
   console.log(currentUser);
   console.log(estados);
+  console.log(todosEstados);
   res.render("index.ejs", {
     estados: estados,
     total: estados.length,
     users: users,
     color: currentUser.color,
+    nombresEstados: todosEstados
   });
 });
 
@@ -167,7 +139,7 @@ app.post("/user", async (req, res) => {
   if (req.body.add === "new") {
     res.render("new.ejs");
   } else {
-    currentUserId = (req.body.user);
+    currentUserId = req.body.user;
     res.redirect("/");
   }
 });
@@ -187,19 +159,15 @@ app.post("/new", async (req, res) => {
 
 app.post("/clear", async (req, res) => {
   try {
-    await connection.query("DELETE FROM estados_visitados WHERE user_id = ?" , [currentUserId])
+    await connection.query("DELETE FROM estados_visitados WHERE user_id = ?", [
+      currentUserId,
+    ]);
     console.log(`Información borrada para el usuario: ${currentUserId}`);
     // await connection.query("DELETE FROM users WHERE id = ?" , [currentUserId])
     // currentUserId = 1;
     res.redirect("/");
-  }
-  catch (err) {
+  } catch (err) {
     console.error("Error trying to delete information of user from db ", err);
     res.status(500).send("Error al tratar de borrar de la base de datos");
   }
-})
-
-
-//TODO Tenemos que agregar la ruta para que se puedan agregar estados a los usuarios
-//TODO se necesitará una query para que consigamos el id del estado cuando el usuario ingrese el nombre del estado
-//TODO después necesitamos una query para insertar ese id del estado con el usuario actual
+});
